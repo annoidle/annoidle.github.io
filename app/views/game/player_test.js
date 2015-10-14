6d7cc0993695178112d7a17de8f518a7245acb54
Jonathan Scher <scher.jonathan@gmail.com>
2015-10-14 19:51:27 +1300

    One class well tested! With Mocks and everything!!!

--- app/views/game/player_test.js
+++ app/views/game/player_test.js
@@ -23,6 +23,27 @@ describe("A suite", function() {
 	});
 
 	describe("When you buy a house", function() {
+		var alertable = null;
+		var alertable2 = null;
+
+		beforeEach(function() {
+			alertable = {
+				housesUpdated: function(value) {},
+				woodUpdated: function(value) {}
+			};
+
+			alertable2 = {
+				housesUpdated: function(value) {},
+				woodUpdated: function(value) {}
+			};
+
+			spyOn(alertable, 'housesUpdated');
+			spyOn(alertable, 'woodUpdated');
+			spyOn(alertable2, 'housesUpdated');
+			spyOn(alertable2, 'woodUpdated');
+		});
+
+
 		it("player gain a house", inject(function(PlayerData) {
 			PlayerData.buyHouses(1);
 			expect(PlayerData.getHouses()).toBe(1);
@@ -31,6 +52,22 @@ describe("A suite", function() {
 		it("player loses 3 WOOD", inject(function(PlayerData) {
 			PlayerData.buyHouses(1);
 			expect(PlayerData.getWood()).toBe(3000 - 3);
+		}));
+
+		it("alerts registed HOUSES observers", inject(function(PlayerData) {
+			PlayerData.registerHouses(alertable);
+			PlayerData.registerHouses(alertable2);
+			PlayerData.buyHouses(1);
+			 expect(alertable.housesUpdated).toHaveBeenCalled();
+			 expect(alertable2.housesUpdated).toHaveBeenCalled();
+		}));
+
+		it("alerts registed WOOD observers", inject(function(PlayerData) {
+			PlayerData.registerWood(alertable);
+			PlayerData.registerWood(alertable2);
+			PlayerData.buyHouses(1);
+			 expect(alertable.woodUpdated).toHaveBeenCalled();
+			 expect(alertable2.woodUpdated).toHaveBeenCalled();
 		}));
 	});
 });
