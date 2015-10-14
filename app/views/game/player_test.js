6f2fda0e5df20d859181ecf35e8e2b4d7ee7331b
Jonathan Scher <scher.jonathan@gmail.com>
2015-10-14 18:42:19 +1300

    we are starting to test for real!!! I'm excited!

--- app/views/game/player_test.js
+++ app/views/game/player_test.js
@@ -4,9 +4,33 @@ describe("A suite", function() {
 
 	beforeEach(angular.mock.module('indexApp.game'));
 
-	describe("A suite", function() {
-		it("contains spec with an expectation", inject(function(PlayerData) {
+	describe("At the start of the application", function() {
+		it("player has 3k GOLD", inject(function(PlayerData) {
 			expect(PlayerData.getGold()).toBe(3000);
+		}));
+		it("player has 3k FOOD", inject(function(PlayerData) {
+			expect(PlayerData.getFood()).toBe(3000);
+		}));
+		it("player has 3k WOOD", inject(function(PlayerData) {
+			expect(PlayerData.getWood()).toBe(3000);
+		}));
+		it("player has 3k TOOLS", inject(function(PlayerData) {
+			expect(PlayerData.getTools()).toBe(3000);
+		}));
+		it("player has 0 HOUSES", inject(function(PlayerData) {
+			expect(PlayerData.getHouses()).toBe(0);
+		}));
+	});
+
+	describe("When you buy a house", function() {
+		it("player gain a house", inject(function(PlayerData) {
+			PlayerData.buyHouses(1);
+			expect(PlayerData.getHouses()).toBe(1);
+		}));
+
+		it("player loses 3 WOOD", inject(function(PlayerData) {
+			PlayerData.buyHouses(1);
+			expect(PlayerData.getWood()).toBe(3000 - 3);
 		}));
 	});
 });
